2df693e320636fe33d44cceb19d7596b7c7a484e
lujain415 <lujainalaamri55@gmail.com>
2025-07-13 23:15:24 +0300

    completed

---
+++ blogger_project/blog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+from .models import Post
+
+def home(request):
+    posts = Post.objects.filter(is_published=True).order_by('-published_at')
+    return render(request, 'blog/home.html', {'posts': posts})
+
+def add_post(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        Post.objects.create(title=title, content=content)
+        return redirect('home')
+    return render(request, 'blog/add_post.html')
