36159aa8379625a1d10bf7bf6309a8ecd8cdcec4
Abdulaziz <abdulaziz.ip33@gmail.com>
2025-07-15 02:21:20 +0300

    adding Update & Delete

--- Blogger/blogs/urls.py
+++ Blogger/blogs/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name="blogs"
 
 urlpatterns= [
-    path("createBlogs/", views.createBlogs_view, name="createBlogs_view")
+    path("createBlogs/", views.createBlogs_view, name="createBlogs_view"),
+    path("detail/<blog_id>/", views.detailBlog_view, name="detailBlog_view" ),
+    path("update/<blog_id>/", views.updateBlog_view, name="updateBlog_view"),
+    path("delete/<blog_id>/", views.deleteBlog_view, name="deleteBlog_view"),
 ]
--- Blogger/blogs/views.py
+++ Blogger/blogs/views.py
@@ -9,18 +9,44 @@ from .models import Blog
 
 def createBlogs_view(request:HttpRequest):
 
-
-
     if request.method =="POST":
         published_at_raw = request.POST.get("published_at")
         published_at = published_at_raw if published_at_raw else timezone.now().date()
 
+        new_blog=Blog(title=request.POST["title"],content=request.POST["content"],is_published = "is_published" in request.POST, published_at=published_at,)
 
-        new_blog=Blog(title=request.POST["title"],content=request.POST["content"],is_published = "is_published" in request.POST, published_at=published_at, image= request.FILES["image"])
+        if "image" in request.FILES:
+            new_blog.image = request.FILES["image"]
+
         new_blog.save()
         return redirect('main:home_view')
-
-
-
     return render(request, "blogs/createBlogs.html")
+
+
+def detailBlog_view(request:HttpRequest,blog_id:int):
+    blog= Blog.objects.get(pk=blog_id)
+
+    return render(request, "blogs/detailBlog.html",{"blog":blog} )
+
+
+def updateBlog_view(request:HttpRequest,blog_id:int):
+    blog= Blog.objects.get(pk=blog_id)
+
+    if request.method =="POST":
+        blog.title=request.POST["title"]
+        blog.content=request.POST["content"]
+        blog.is_published = "is_published" in request.POST
+        blog.published_at=request.POST["published_at"]
+        if "image" in request.FILES: blog.image= request.FILES["image"]
+        blog.save()
+
+        return redirect("blogs:detailBlog_view", blog_id=blog.id)
+
+    return render(request, "blogs/updateBlog.html",{"blog":blog} )
+
+def deleteBlog_view(request:HttpRequest,blog_id:int):
+    blog= Blog.objects.get(pk=blog_id)
+    blog.delete()
+
+    return redirect('main:home_view')
 
