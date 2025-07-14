24089f28363a5b2ba64b190091dfa4317e35b1d1
lujain415 <lujainalaamri55@gmail.com>
2025-07-14 23:26:58 +0300

    completed

--- blogger_project/blog/urls.py
+++ blogger_project/blog/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('add/', views.add_post, name='add_post'),
+    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('post/<int:post_id>/edit/', views.edit_post, name='edit_post'),
+    path('post/<int:post_id>/delete/', views.delete_post, name='delete_post'),
+
+
 ]
--- blogger_project/blog/views.py
+++ blogger_project/blog/views.py
@@ -9,6 +9,36 @@ def add_post(request):
     if request.method == 'POST':
         title = request.POST.get('title')
         content = request.POST.get('content')
-        Post.objects.create(title=title, content=content)
+        image = request.FILES.get('image')
+        Post.objects.create(title=title, content=content,image=image)
         return redirect('home')
     return render(request, 'blog/add_post.html')
+
+
+def post_detail(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def edit_post(request, post_id):
+    post = Post.objects.get(id=post_id)
+    
+    if request.method == 'POST':
+        post.title = request.POST.get('title')
+        post.content = request.POST.get('content')
+
+        if request.FILES.get('image'): 
+            post.image = request.FILES.get('image')
+
+        post.save()
+        return redirect('post_detail', post_id=post.id)
+
+    return render(request, 'blog/edit_post.html', {'post': post})
+
+def delete_post(request, post_id):
+    post = Post.objects.get(id=post_id)
+
+    if request.method == 'POST':
+        post.delete()
+        return redirect('home')
+
+    return render(request, 'blog/delete_post.html', {'post': post})
