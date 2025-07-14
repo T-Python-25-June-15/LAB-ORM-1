b300f4a22c1f784a0f36fa0f98c685d6e933b643
sarah <sarah@sarahs-MacBook-Pro.local>
2025-07-14 20:23:53 +0300

    Add post images, details page, update and delete posts

--- Blogger/main/urls.py
+++ Blogger/main/urls.py
@@ -5,5 +5,10 @@ app_name = "main"
 
 urlpatterns = [
     path("", views.home_view, name="home_view"),
-    path("post/new/", views.add_post, name="add_post")
+    path("post/new/", views.add_post, name="add_post"),
+    path("post/detail/<post_id>/", views.detail_post_view, name="detail_post_view"),
+    path("post/update/<post_id>/", views.update_post_view, name="update_post_view"),
+    path("post/delete/<post_id>/", views.delete_post_view, name="delete_post_view")
+
+
 ] 
--- Blogger/main/views.py
+++ Blogger/main/views.py
@@ -7,7 +7,32 @@ def home_view(request:HttpRequest):
     return render(request,"main/index.html",{"posts":posts})
 
 def add_post(request:HttpRequest):
-    if request.method == "POST":
+    if request.method == "POST" and "image" in  request.FILES:
+        new_post = Post(title = request.POST["title"], content = request.POST["content"], image = request.FILES["image"])
+        new_post.save()
+        return redirect("main:home_view")
+    elif request.method == "POST" and "image" not in request.FILES:
         new_post = Post(title = request.POST["title"], content = request.POST["content"])
         new_post.save()
+        return redirect("main:home_view")
     return render(request,"main/add_post.html")
+
+def detail_post_view(request:HttpRequest, post_id:int):
+    post = Post.objects.get(id = post_id)
+    return render(request,"main/detail_post.html", {"post" : post})
+
+def update_post_view(request:HttpRequest, post_id:int):
+    post = Post.objects.get(id = post_id)
+    if request.method == "POST":
+        post.title = request.POST["title"]
+        post.content = request.POST["content"]
+        if "image" in  request.FILES:
+            post.image = request.FILES["image"]
+        post.save()
+        return redirect("main:detail_post_view", post_id=post.id)
+    return render(request,"main/update_post.html", {"post" : post})
+
+def delete_post_view(request:HttpRequest, post_id:int):
+    post = Post.objects.get(id = post_id)
+    post.delete()
+    return redirect("main:home_view")
