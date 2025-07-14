19cde861420ce02c44a766f925abd6d6500d8dc6
meshari alharbi <109522810+alharbisa24@users.noreply.github.com>
2025-07-14 12:37:08 +0300

    update-delete-view, images

--- Blogger/main/urls.py
+++ Blogger/main/urls.py
@@ -6,5 +6,8 @@ app_name="main"
 
 urlpatterns = [
     path('', views.main_view, name="main_view" ),
-    path('add_post/',views.add_post, name="add_post_view")
+    path('add_post/',views.add_post, name="add_post_view"),
+    path('post/<int:id>/',views.post_details_view, name="post_details_view"),
+    path('post/<int:id>/edit/',views.post_edit_view, name="post_edit_view"),
+    path('post/<int:id>/delete/',views.post_delete_view, name="post_delete_view")
 ] 
--- Blogger/main/views.py
+++ Blogger/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpRequest, HttpResponse
 from main.models import Post
 
@@ -13,8 +13,44 @@ def add_post(request:HttpRequest):
 
     if request.POST:
         is_publish = True if request.POST['is_published'] == 'show' else False
-        post = Post(title=request.POST['title'],content=request.POST['content'],is_published=is_publish)
+        post = Post(title=request.POST['title'],content=request.POST['content'],is_published=is_publish,image = request.FILES['image'])
         post.save()
+        return redirect('main:main_view')
 
 
     return render(request, "add_post.html")
+
+
+
+def post_details_view(request:HttpRequest,id:int):
+    post = Post.objects.get(pk=id)
+    return render(request,'post_details.html',{
+        "post":post
+    })
+
+
+def post_edit_view(request:HttpRequest, id:int):
+    post = Post.objects.get(pk=id)
+
+    if request.POST:
+        post.title = request.POST['title']
+        post.content = request.POST['content']
+        post.is_published = True if request.POST['is_published'] == 'show' else False
+        post.published_at = request.POST['published_at']
+        if post.image in request.FILES:
+            post.image = request.FILES['image']
+        post.save()
+        return redirect('main:main_view')
+
+
+
+    return render(request,'edit_post.html',{
+        "post":post
+    })
+
+
+def post_delete_view(request:HttpRequest, id:int):
+    post = Post.objects.get(pk=id)
+    if post:
+        post.delete()
+        return redirect('main:main_view')
