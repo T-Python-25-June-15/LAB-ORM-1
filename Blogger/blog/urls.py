0e6914d165d72786480a3c54f9fec6b512b90841
Your Name <you@example.com>
2025-07-14 14:55:30 +0300

    Adding update and delete features

--- Blogger/blog/urls.py
+++ Blogger/blog/urls.py
@@ -6,6 +6,9 @@ app_name = 'blog'
 
 urlpatterns = [
     path("create/", views.create_post_view , name="create_post_view"),
-    path("delete/", views.delete_all_objects_view , name="delete_all_objects_view"),
+    path("deleteall/", views.delete_all_objects_view , name="delete_all_objects_view"),
+    path("details/<blog_id>/" , views.blog_detail_view ,name="blog_detail_view" ),
+    path("update/<blog_id>/" , views.blog_update_view ,name="blog_update_view" ),
+    path("delete/<blog_id>/" , views.delete_blog_view ,name="delete_blog_view" )
 
  ]
--- Blogger/blog/views.py
+++ Blogger/blog/views.py
@@ -13,13 +13,54 @@ def create_post_view(request: HttpRequest):
             title=request.POST["title"],
             content=request.POST["content"],
             is_published = request.POST.get("is_published") == "on",
-            published_at= datetime.now()
+            published_at= datetime.now(),
+            image=request.FILES["image"]
         )
         new_post.save()
-
+        return redirect('main:home_view')
+    
     return render(request, "blog/create.html")
+
+def blog_update_view(request: HttpRequest, blog_id: int):
+
+    blog = Post.objects.get(pk=blog_id)
+
+    if request.method == "POST":
+        blog.title = request.POST["title"]
+        blog.content = request.POST["content"]
+        blog.is_published = request.POST.get("is_published") == "on"
+        blog.published_at = datetime.now()
+        
+        if "image" in request.FILES : blog.image = request.FILES["image"]
+        
+        
+        blog.save()
+        
+        
+        
+        
+        return redirect('blog:blog_detail_view' , blog_id = blog.id)
+    
+    return render(request, "blog/blog_update.html", {"blog": blog})
+
+
 
 def delete_all_objects_view(request: HttpRequest):
     Post.objects.all().delete() 
 
     return render(request, "blog/create.html")
+
+def delete_blog_view(request: HttpRequest , blog_id: int):
+
+    blog = Post.objects.get(pk=blog_id)
+
+    blog.delete()
+    return redirect('main:home_view')
+
+
+
+def blog_detail_view(request: HttpRequest , blog_id :int):
+     
+    blog = Post.objects.get(pk=blog_id)
+    print(blog.title)
+    return render(request, "blog/blog_detail.html", {"blog" : blog})
