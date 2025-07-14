e2a015e52b8ec813adb15ad057dfc20704f6c2df
Abdulaziz <abdulaziz.ip33@gmail.com>
2025-07-14 22:34:55 +0300

    uploading media files to blog app

--- Blogger/blogs/models.py
+++ Blogger/blogs/models.py
@@ -9,3 +9,4 @@ class Blog(models.Model):
     content= models.TextField()
     is_published = models.BooleanField(default=True)
     published_at = models.DateField(default=timezone.now)
+    image= models.ImageField(upload_to="images/", default="images/default.jpg")
--- Blogger/blogs/views.py
+++ Blogger/blogs/views.py
@@ -16,8 +16,9 @@ def createBlogs_view(request:HttpRequest):
         published_at = published_at_raw if published_at_raw else timezone.now().date()
 
 
-        new_blog=Blog(title=request.POST["title"],content=request.POST["content"],is_published = "is_published" in request.POST, published_at=published_at)
+        new_blog=Blog(title=request.POST["title"],content=request.POST["content"],is_published = "is_published" in request.POST, published_at=published_at, image= request.FILES["image"])
         new_blog.save()
+        return redirect('main:home_view')
 
 
 
