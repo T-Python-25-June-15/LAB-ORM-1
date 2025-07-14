9f2f4872ae460ae02bf3c103f7c0b259f88e6c76
shatha <shatha25200@gmail.com>
2025-07-14 13:55:02 +0300

    Added Detail Page, Update & Delete

--- Blogger/main/urls.py
+++ Blogger/main/urls.py
@@ -5,5 +5,8 @@ app_name = 'main'
 
 urlpatterns = [
     path('',views.home_views, name='home_views'),
-    path('add_blog/',views.add_blog_views, name='add_blog_views')
+    path('add_blog/',views.add_blog_views, name='add_blog_views'),
+    path('detail/<blog_id>/',views.detail_views, name='detail_views'),
+    path('update/<blog_id>/',views.update_views, name='update_views'),
+    path('delete/<blog_id>/',views.delete_views, name='delete_views'),
 ]
--- Blogger/main/views.py
+++ Blogger/main/views.py
@@ -13,3 +13,26 @@ def add_blog_views(request:HttpRequest):
         new_post.save()
         return redirect('main:home_views')
     return render(request, 'main/add_page.html')
+
+def detail_views(request:HttpRequest, blog_id:int):
+    blog = Post.objects.get(pk=blog_id)
+    return render(request, 'main/blog_details.html', {"blog":blog})
+
+
+def update_views(request:HttpRequest, blog_id:int):
+    blog = Post.objects.get(pk=blog_id)
+    if request.method == "POST":
+        blog.title = request.POST["title"]
+        blog.content = request.POST["content"]
+        blog.published_at = request.POST["published_at"]
+        if "images" in request.FILES:
+            blog.images = request.FILES["images"]
+        blog.save()
+        return redirect('main:detail_views', blog_id = blog.id)
+
+    return render(request, 'main/update.html', {"blog":blog})
+
+def delete_views(request:HttpRequest, blog_id:int):
+    blog = Post.objects.get(pk=blog_id)
+    blog.delete()
+    return redirect('main:home_views')
