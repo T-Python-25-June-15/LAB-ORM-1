ebbdd7afa63b92205a0317ef9856d9ecb18e5b97
Hesham <HeshamAlsadan@gmail.com>
2025-07-15 08:20:08 +0300

    update feature

--- Blogger/main/views.py
+++ Blogger/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 from main.models import Post
 
@@ -14,4 +14,24 @@ def create_post_view(request: HttpRequest) -> HttpResponse:
     if request.method == 'POST':
         post = Post(title=request.POST.get('title') , content=request.POST.get('content') , is_published= bool(request.POST.get('is_published')), published_at= request.POST.get('published_at'), poster=request.FILES.get('poster'))
         post.save()
+    return render(request, 'main/new_post.html')
+
+
+def show_details_view(request: HttpRequest, id_post: int) -> HttpResponse:
+    post = Post.objects.get(pk=id_post)
+    return render(request, 'main/details.html', context={'post': post})
+
+
+def update_post_view(request: HttpRequest, id_post):
+    if request.method == 'POST':
+        post = Post.objects.get(pk=id_post)
+        post.title = request.POST.get('title')
+        post.content=request.POST.get('content')
+        post.is_published= bool(request.POST.get('is_published'))
+        post.published_at= request.POST.get('published_at')
+        post.poster=request.FILES.get('poster')
+
+        if request.FILES.get('poster'): post.poster = request.FILES.get('poster')
+        post.save()
+        return redirect('main:home_view')
     return render(request, 'main/new_post.html')
