57915dd470cdb2b8a4a5aad58cbdbb3efd7928da
Saif <sknalyami@gmail.com>
2025-07-15 10:11:02 +0300

    Done | Saif

--- Blogger/blog/urls.py
+++ Blogger/blog/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 from . import views
+from . models import Post
 
 app_name = "blog"
 
 urlpatterns = [
     path("", views.home_view, name="home_view"),
     path("add/post/", views.add_post, name="add_post"),
+    path("detail/<post_id>/", views.post_details_view, name="post_details_view"),
+    path("update/<post_id>/", views.post_update_view, name="post_update_view"),
+    path("delete/<post_id>/", views.post_delete_view , name="post_delete_view"),
+
 
 ]
--- Blogger/blog/views.py
+++ Blogger/blog/views.py
@@ -1,19 +1,23 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 from .models import Post
+from django.utils import timezone
+
+
 
 def home_view(request: HttpRequest):
     posts = Post.objects.all()
     return render(request, "blog/index.html", context={"posts" : posts})
 
+
+
 def add_post(request: HttpRequest):
     if request.method == "POST":
-        title = request.POST.get("title")
-        content = request.POST.get("content")
         new_post = Post(
-            title=title,
-            content=content,
-            is_published=True  
+            title=request.POST["title"],
+            content=request.POST["content"],
+            is_published=True,
+            poster=request.FILES["poster"]
         )
         new_post.save()
         return redirect("blog:home_view")
@@ -21,4 +25,36 @@ def add_post(request: HttpRequest):
     return render(request, "blog/addPost.html")
 
 
+
+def post_details_view (request: HttpRequest, post_id: int ):
+    post = Post.objects.get(pk=post_id)
+    return render(request, "blog/post_details.html", context={"post" : post})
+
+
+def post_update_view (request: HttpRequest, post_id: int):
+
+    post = Post.objects.get(pk=post_id)
+
+    if request.method == "POST":
+        post.title = request.POST["title"]
+        post.content = request.POST["content"]
+        if "poster" in request.FILES: 
+            post.poster = request.FILES["poster"]
+        post.published_at = timezone.now()
+
+        post.save()
+        return redirect("blog:post_details_view", post_id=post.id)
+        
+
+    return render(request, "blog/post_update.html", context={"post" : post} )
+
+
+
+
+def post_delete_view (request: HttpRequest, post_id: int):
+
+    post = Post.objects.get(pk=post_id)
+    post.delete()
+
+    return redirect ("blog:home_view")
 
