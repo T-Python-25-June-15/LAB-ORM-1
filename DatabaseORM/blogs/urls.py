9e7c9354b673ad9c87cc03153c3e23a9d393167d
Nawaf Kamal <xp.mo91@gmail.com>
2025-07-15 13:43:46 +0300

    done all

--- DatabaseORM/blogs/urls.py
+++ DatabaseORM/blogs/urls.py
@@ -5,4 +5,7 @@ app_name = "blogs"
 
 urlpatterns = [
     path('',views.add_blog, name="add_blog_new"),
+    path('detail/<blog_id>/',views.blog_detail_view, name="blog_detail_view"),
+    path('update/<blog_id>/',views.blog_update_view, name="blog_update_view"),
+    path('delete/<blog_id>/',views.blog_delete_view, name="blog_delete_view"),
 ]
--- DatabaseORM/blogs/views.py
+++ DatabaseORM/blogs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpRequest,HttpResponse
 
 from .models import Blogger
@@ -19,8 +19,47 @@ def add_blog(request:HttpRequest):
     if request.POST["is_published"] == "0":
       status = False
       
-    new_blog = Blogger(title=request.POST["title"], content=request.POST["content"], is_published = status, poster=request.FILES["poster"])
+    new_blog = Blogger(
+      title=request.POST["title"],
+      content=request.POST["content"],
+      is_published = status,
+      poster=request.FILES["poster"])
     new_blog.save()
 
 
   return render(request, 'main/add.html')
+
+
+def blog_detail_view(request:HttpRequest, blog_id:int):
+
+  blog = Blogger.objects.get(pk=blog_id)
+
+  return render(request, "main/blog_detail.html", {"bloge":blog})
+
+def blog_update_view(request:HttpRequest, blog_id:int):
+
+  blog = Blogger.objects.get(pk=blog_id)
+  
+  if request.method == "POST":
+    status = True
+
+    if request.POST["is_published"] == "0":
+      status = False
+      
+    blog.title=request.POST["title"]
+    blog.content=request.POST["content"]
+    blog.is_published = status
+    blog.poster=request.FILES["poster"]
+    blog.save()
+
+  
+
+  return render(request, "main/update.html", {"bloge":blog})
+
+def blog_delete_view(request:HttpRequest, blog_id:int):
+
+  blog = Blogger.objects.get(pk=blog_id)
+  blog.delete()
+
+  # return render(request, "")
+  return redirect('/')
