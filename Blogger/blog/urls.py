cc41d97cd1fa63d070eaf8955fb81739dd3b3dd1
Abdulrahman <a3.alsuhaymi@gmail.com>
2025-07-15 08:58:11 +0300

    done

--- Blogger/blog/urls.py
+++ Blogger/blog/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('add/', views.add_post, name='add_post'),
+    path('detail/<post_id>/', views.post_detail, name='post_detail'),
+    path('update/<post_id>/', views.post_update, name='post_update'),
+    path('delete/<post_id>/', views.post_delete, name='post_delete')
 ] 
--- Blogger/blog/views.py
+++ Blogger/blog/views.py
@@ -13,13 +13,38 @@ def add_post(request:HttpRequest):
     if request.method == 'POST':
         title = request.POST.get('title', '')
         content = request.POST.get('content', '')
-        
+        image = request.FILES.get('image','')
+
         post = Post.objects.create(
             title=title,
             content=content,
             published_at=timezone.now(),
-            is_published=True
+            is_published=True,
+            image=image
         )
         return redirect('home')
     
     return render(request, 'blog/add_post.html')
+
+
+def post_detail(request:HttpRequest, post_id:int):
+    post = Post.objects.get(pk=post_id)
+
+    return render(request, 'blog/post_detail.html',{"post": post})
+
+def post_update(request:HttpRequest, post_id:int):
+    post = Post.objects.get(pk=post_id)
+    if request.method == "POST":
+        post.title = request.POST["title"]
+        post.content = request.POST["content"]
+        if "image" in request.FILES: post.image = request.FILES["image"]
+        post.save()
+        return redirect('post_detail',post_id=post.id)
+
+
+    return render(request, 'blog/post_update.html',{"post": post})
+
+def post_delete(request:HttpRequest, post_id:int):
+    post = Post.objects.get(pk=post_id)
+    post.delete()
+    return redirect('home')
