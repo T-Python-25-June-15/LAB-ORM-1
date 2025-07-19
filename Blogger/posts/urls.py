4b415c5513e57f7e5586e27c4df363281581fe4d
Moh0py <1vipmoh@gmail.com>
2025-07-19 03:07:45 +0300

    ALL

--- Blogger/posts/urls.py
+++ Blogger/posts/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),  
+    path('details/<int:post_id>/', views.details_post, name='details_post'),
     path('delete/<int:post_id>/', views.delete_post, name='delete_post'),
     path('update/<int:post_id>/', views.update_post, name='update_post'),
     path('add/', views.add_post, name='add_post'), 
--- Blogger/posts/views.py
+++ Blogger/posts/views.py
@@ -5,31 +5,52 @@ from .forms import PostForm
 
 
 def home(request):
-    posts = Post.objects.all().order_by('-published_at')
+    posts = Post.objects.filter( media__gt=1, is_published=True).order_by('-published_at')
     return render(request, 'posts/home.html', {'posts': posts})
 
 def add_post(request):
-    if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES) 
-        if form.is_valid():
-            form.save()
-            return redirect('home')
+    try:
+        if request.method == 'POST':
+            form = PostForm(request.POST, request.FILES)
+            if form.is_valid():
+                form.save()
+                return redirect('home')
+    except Exception as e:
+        print(f"Error adding post: {e}")
     else:
         form = PostForm()
     return render(request, 'posts/add_post.html', {'form': form})
 
+
 def delete_post(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    post.delete()
-    return redirect('home') 
+    try:
+        post = get_object_or_404(Post, id=post_id)
+        post.delete()
+        return redirect('home')
+    except Exception as e:
+        print(f"Error deleting post: {e}")
+        return redirect('home')
+
 
 def update_post(request, post_id):
     post = get_object_or_404(Post, id=post_id)
+
     if request.method == 'POST':
-        form = PostForm(request.POST, request.FILES, instance=post)  
+        form = PostForm(request.POST, request.FILES, instance=post)
+        
         if form.is_valid():
             form.save()
-            return redirect('home')
+            return redirect('details_post', post_id=post.id)
     else:
-        form = PostForm(instance=post)  
+        form = PostForm(instance=post)
+
     return render(request, 'posts/update_post.html', {'form': form, 'post': post})
+
+
+def details_post(request, post_id):
+    post = Post.objects.get(id=post_id)
+
+    if not post.is_published:
+        return redirect('home') 
+
+    return render(request, 'posts/deatils_post.html', {'post': post})
